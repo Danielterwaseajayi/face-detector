8a171283bd0a1814425770b11e1b66399720414d
Danielterwaseajayi <hemmbadon@gmail.com>
2025-07-03 10:29:03 +0100

    picture.py

--- picture.py
+++ picture.py
@@ -1,14 +1,10 @@
 import cv2
 
-face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-image = cv2.imread('Ajayi Daniel.jpg')
+image = cv2.imread(r"C:\Users\Daniel Terwase Ajayi\PycharmProjects\pythonProject33\Daniel.jpg")
 
 grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# cv2.imshow("Grey", grey)
-
-# cv2.waitKey(0)
 
 faces = face_haar_cascade.detectMultiScale(grey, 1.1, 4)
 for (x, y, w, h) in faces:
@@ -16,3 +12,5 @@ for (x, y, w, h) in faces:
 
 cv2.imshow("Faces", image)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+
